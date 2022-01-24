b138c37a379373f651eab8134fe4cb99a2bb9be0
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-01-24 15:52:28 +0300

    F

---
+++ 11.js
@@ -0,0 +1,12 @@
+var keyPressed = "N";
+var points = 142;
+var level;
+if (keyPressed == "Y" ||
+ (points > 100 && points < 200)) {
+ level = 2;
+} else {
+ level = 1;
+}
+Чему равно значение level?
+true
+true
